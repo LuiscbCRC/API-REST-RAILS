72cb3d691c2126fda50d87848157f79fe99f0fe8
Luis Castillo <luiscb04@gmail.com>
2021-02-11 08:43:55 -0600

    Adding CRUD functionalities to models

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,44 @@
+# frozen_string_literal: true
+
 class OrdersController < ApplicationController
+  before_action :set_order, only: %i[show update destroy]
+
+  # GET /orders
+  def index
+    @orders = Order.all
+    json_response(@orders)
+  end
+
+  # POST /orders
+  def create
+    @order = Order.create!(order_params)
+    json_response(@order, :created)
+  end
+
+  # GET /orders/:id
+  def show
+    json_response(@order)
+  end
+
+  # PUT /orders/:id
+  def update
+    @order.update(order_params)
+    head :no_content
+  end
+
+  # DELETE /orders/:id
+  def destroy
+    @order.destroy
+    head :no_content
+  end
+
+  private
+
+  def order_params
+    params.permit(:amount, :user_id, :payment_status_id, :order_status_id, :delivery_date_id, :payment_date_id)
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
 end
